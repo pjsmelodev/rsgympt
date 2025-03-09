293d86e5e4bcdf8cd838e2fbd150b6b6bab82695
pjsm <pjsm.lesi@gmail.com>
2025-03-09 22:11:07 +0000

    CREATE utilitarian class

---
+++ RSGymPT/RSGymPT/Utils/Utility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSGymPT.Utils
+{
+    public static class Utility
+    {
+        public static void PauseTerminal()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
